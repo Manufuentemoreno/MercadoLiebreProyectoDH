40f2ec4ed4e7db9bf2bc4035b6ffe0d74dacceb9
Manufuentemoreno <manu-u@live.com.ar>
2022-05-21 21:42:58 -0300

    sigo trabajando con formularios

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const app = express();
 const publicPath = path.resolve(__dirname,"./public");
 app.use( express.static(publicPath) );
 
-app.listen(3000, () => console.log("Servidor ejecutándose en el puerto 3000"));
+const PORT = 3000
+app.listen(PORT, () => console.log("Servidor ejecutándose en el puerto ",PORT));
 
 app.get("/",(req, res) => {
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
+});
+
+app.get("/CreaTuCuenta",(req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
 });
