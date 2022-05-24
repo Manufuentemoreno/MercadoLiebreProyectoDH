17436c9a834dad35fedbbe6ff51fa79bf7c552bc
Manuel Fuente Moreno <manu-u@live.com.ar>
2022-05-24 00:55:51 -0300

    lista para cargar a heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const publicPath = path.resolve(__dirname,"./public");
 app.use( express.static(publicPath) );
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Servidor ejecutándose en el puerto ",PORT));
 
 app.get("/",(req, res) => {
