7aa9c7ce7608da62cf3f2bb4fe53358571de8815
Manuel Fuente Moreno <manu-u@live.com.ar>
2022-05-26 08:39:01 -0300

    renombro la carpeta CSS a css

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ const path = require("path");
 
 const app = express();
 
-// const publicPath = path.resolve(__dirname,"./public");
-// app.use( express.static(publicPath) );
-
-app.use(express.static(path.join(__dirname, "public")));
-
+const publicPath = path.resolve(__dirname,"./public");
+app.use( express.static(publicPath) );
 
 
 const PORT = process.env.PORT || 3000;
